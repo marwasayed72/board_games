b59d7a717b50f49dccf36e0529ebe14263086974
An-as4799 <160598845+An-as4799@users.noreply.github.com>
2024-12-12 19:51:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main() {
         cout<<"Enter your choice: "<<endl;
         string ccc ;
         cin>>ccc;
-        while(ccc!="1"&&ccc!="2"&&ccc!="3" &&ccc!="4" && ccc!="5" && ccc!="6" &&ccc!="7" ){
+        while(ccc!="1"&&ccc!="2"&&ccc!="3" &&ccc!="4" && ccc!="5" && ccc!="6" &&ccc!="7"&&ccc!="8"){
             cout<<"Invalid input, please, try again: "<<endl;
             cin>>ccc;
         }
@@ -353,6 +353,64 @@ int main() {
     }
 
         }
+        else if(ccc==7){
+            Player<char>* players[2];
+    Board<char>* B = new Pyramic_Board<char>();
+    string playerXName, player2Name;
+
+    cout << "Welcome to Pyramic X-O Game. :))\n";
+
+    // Set up player 1
+    cout << "Enter Player X name: ";
+    cin >> playerXName;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+            players[0] = new Pyramic_Player<char>(playerXName, 'X');
+            break;
+        case 2:
+            players[0] = new Pyramic_Random_Player<char>('X');
+            break;
+        default:
+            cout << "Invalid choice for Player 1. Exiting the game.\n";
+            return 1;
+    }
+
+    // Set up player 2
+    cout << "Enter Player 2 name: ";
+    cin >> player2Name;
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+            players[1] = new Pyramic_Player<char>(player2Name, 'O');
+            break;
+        case 2:
+            players[1] = new Pyramic_Random_Player<char>('O');
+            break;
+        default:
+            cout << "Invalid choice for Player 2. Exiting the game.\n";
+            return 1;
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> x_o_game(B, players);
+    x_o_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+            
+            
         else{
             exit(0);
         }
